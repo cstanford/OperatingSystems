5b2ba7286a08289eb59ca61d87114af1059c0a64
Alex Fontenot <guiri.alien@gmail.com>
2016-01-29 14:02:48 -0600

    Cleans up the input verification code for task 2

--- code/threads/inputidentification.h
+++ code/threads/inputidentification.h
@@ -2,8 +2,23 @@
 #include "string.h"
 
 //Code edited by Joseph Fontenot and Connor Stanford
+enum input_type {
+    INTEGER,
+    NEG_INTEGER,
+    DECIMAL,
+    NEG_DECIMAL,
+    CHAR_STRING
+};
 
+bool isInteger(char* input);
+bool isNegativeInteger(char* input);
+bool isDecimal(char* input);
+bool isNegativeDecimal(char *input);
+void InputIdentification(int dummyParameter);
 
+input_type CheckType(char *input);
+
+//Code edited by Joseph Fontenot and Connor Stanford
 bool isInteger(char* input){
     if(*input == '\0') return false;
     for(; *input != '\0'; input++){
@@ -12,6 +27,7 @@ bool isInteger(char* input){
     }
     return true;
 }
+
 bool isNegativeInteger(char* input){
     if(*input == '-'){
         bool isInt = isInteger(++input);
@@ -19,6 +35,7 @@ bool isNegativeInteger(char* input){
     }
     return false;
 }
+
 bool isDecimal(char* input){
     char *temp = input;
     int periodIndex = 0;
@@ -45,8 +62,7 @@ bool isDecimal(char* input){
     return (isInteger(left) && isInteger(right));
 }
 
-bool isNegativeDecimal(char *input)
-{
+bool isNegativeDecimal(char *input){
     if(*input == '-'){
         bool isNegDec = isDecimal(++input);
         return isNegDec;
@@ -54,47 +70,50 @@ bool isNegativeDecimal(char *input)
     return false;
 }
 
-bool isCharString(char *input)
-{
+//enum input types defined in header
+input_type CheckType(char *input) {
     bool isInt = isInteger(input);
     bool isNegInt = isNegativeInteger(input);
     bool isDec = isDecimal(input);
     bool isNegDec = isNegativeDecimal(input);
+    bool isCharString = !(isInt || isNegInt || isDec || isNegDec);
 
-    if ( isInt || isNegInt || isDec ||isNegDec)
-    {
-        return false;
-    }
-    return true;
-
+    if(isInt) return INTEGER;
+    else if(isNegInt) return NEG_INTEGER;
+    else if(isDec) return DECIMAL;
+    else if(isNegDec) return NEG_DECIMAL;
+    else if(isCharString) return CHAR_STRING;
 }
 
+void PrintType(input_type t){
+    switch(t){
+    case INTEGER:
+        printf("Integer\n");
+        break;
+    case NEG_INTEGER:
+        printf("Negative Integer\n");
+        break;
+    case DECIMAL:
+        printf("Decimal\n");
+        break;
+    case NEG_DECIMAL:
+        printf("Negative Decimal\n");
+        break;
+    case CHAR_STRING:
+        printf("Character String\n");
+        break;
+    };
+}
 
-void InputIdentification(int dummyParameter) // dummyParameter only here to satisfy forked 
-{                                            // thread parameters.
+// dummyParameter only here to satisfy forked 
+// thread parameters.
+void InputIdentification(int dummyParameter){
     char input[256];
-
 
     printf("Please enter a value less than 256 characters: ");
     scanf("%s", input);
 
-    bool isDec = isDecimal(input);
-    bool isNegDec = isNegativeDecimal(input);
-    bool isInt = isInteger(input);
-    bool isNegInt = isNegativeInteger(input);
-    bool isCharStr = isCharString(input);
-
-    if (isCharStr)
-        printf("Character String\n");
-    else if (isNegDec)
-        printf("Negative Decimal\n");
-    else if (isNegInt)
-        printf("Negative Integer\n");
-    else if (isDec)
-        printf("Decimal\n");
-    else
-        printf("Integer\n");
-
+    PrintType(CheckType(input));
 
   /****** Used for testing ************  
     printf("NegInt: %d\n", isNegInt);
@@ -103,8 +122,4 @@ void InputIdentification(int dummyParameter) // dummyParameter only here to sati
     printf("Dec:    %d\n", isDec);
     printf("String: %d\n", isCharStr);
   *************************************/
-
-
-
 }
-
