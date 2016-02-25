b67df107de77ba9becb0124c2f7ed5f41de30ef1
cstanford <connorgstanford@gmail.com>
2016-02-25 16:27:08 -0600

    added updated exception.cc file.

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -21,9 +21,19 @@
 // All rights reserved.  See copyright.h for copyright notice and limitation 
 // of liability and disclaimer of warranty provisions.
 
+#include <stdio.h>        // FA98
 #include "copyright.h"
 #include "system.h"
 #include "syscall.h"
+#include "addrspace.h"   // FA98
+#include "sysdep.h"   // FA98
+
+// begin FA98
+
+static int SRead(int addr, int size, int id);
+static void SWrite(char *buffer, int size, int id);
+
+// end FA98
 
 //----------------------------------------------------------------------
 // ExceptionHandler
@@ -51,13 +61,170 @@
 void
 ExceptionHandler(ExceptionType which)
 {
-    int type = machine->ReadRegister(2);
+	int type = machine->ReadRegister(2);
 
-    if ((which == SyscallException) && (type == SC_Halt)) {
-	DEBUG('a', "Shutdown, initiated by user program.\n");
-   	interrupt->Halt();
-    } else {
-	printf("Unexpected user mode exception %d %d\n", which, type);
-	ASSERT(FALSE);
-    }
+	int arg1 = machine->ReadRegister(4);
+	int arg2 = machine->ReadRegister(5);
+	int arg3 = machine->ReadRegister(6);
+	int Result;
+	int i, j;
+	char *ch = new char [500];
+
+	switch ( which )
+	{
+	case NoException :
+		break;
+	case SyscallException :
+
+		// for debugging, in case we are jumping into lala-land
+		// Advance program counters.
+		machine->registers[PrevPCReg] = machine->registers[PCReg];
+		machine->registers[PCReg] = machine->registers[NextPCReg];
+		machine->registers[NextPCReg] = machine->registers[NextPCReg] + 4;
+
+		switch ( type )
+		{
+
+		case SC_Halt :
+			DEBUG('t', "Shutdown, initiated by user program.\n");
+			interrupt->Halt();
+			break;
+
+			
+		case SC_Read :
+			if (arg2 <= 0 || arg3 < 0){
+				printf("\nRead 0 byte.\n");
+			}
+			Result = SRead(arg1, arg2, arg3);
+			machine->WriteRegister(2, Result);
+			DEBUG('t',"Read %d bytes from the open file(OpenFileId is %d)",
+			arg2, arg3);
+			break;
+
+		case SC_Write :
+			for (j = 0; ; j++) {
+				if(!machine->ReadMem((arg1+j), 1, &i))
+					j=j-1;
+				else{
+					ch[j] = (char) i;
+					if (ch[j] == '\0') 
+						break;
+				}
+			}
+			if (j == 0){
+				printf("\nWrite 0 byte.\n");
+				// SExit(1);
+			} else {
+				DEBUG('t', "\nWrite %d bytes from %s to the open file(OpenFileId is %d).", arg2, ch, arg3);
+				SWrite(ch, j, arg3);
+			}
+			break;
+
+			default :
+			//Unprogrammed system calls end up here
+			break;
+		}         
+		break;
+
+	case ReadOnlyException :
+		puts ("ReadOnlyException");
+		if (currentThread->getName() == "main")
+		ASSERT(FALSE);  //Not the way of handling an exception.
+		//SExit(1);
+		break;
+	case BusErrorException :
+		puts ("BusErrorException");
+		if (currentThread->getName() == "main")
+		ASSERT(FALSE);  //Not the way of handling an exception.
+		//SExit(1);
+		break;
+	case AddressErrorException :
+		puts ("AddressErrorException");
+		if (currentThread->getName() == "main")
+		ASSERT(FALSE);  //Not the way of handling an exception.
+		//SExit(1);
+		break;
+	case OverflowException :
+		puts ("OverflowException");
+		if (currentThread->getName() == "main")
+		ASSERT(FALSE);  //Not the way of handling an exception.
+		//SExit(1);
+		break;
+	case IllegalInstrException :
+		puts ("IllegalInstrException");
+		if (currentThread->getName() == "main")
+		ASSERT(FALSE);  //Not the way of handling an exception.
+		//SExit(1);
+		break;
+	case NumExceptionTypes :
+		puts ("NumExceptionTypes");
+		if (currentThread->getName() == "main")
+		ASSERT(FALSE);  //Not the way of handling an exception.
+		//SExit(1);
+		break;
+
+		default :
+		//      printf("Unexpected user mode exception %d %d\n", which, type);
+		//      if (currentThread->getName() == "main")
+		//      ASSERT(FALSE);
+		//      SExit(1);
+		break;
+	}
+	delete [] ch;
 }
+
+
+static int SRead(int addr, int size, int id)  //input 0  output 1
+{
+	char buffer[size+10];
+	int num,Result;
+
+	//read from keyboard, try writing your own code using console class.
+	if (id == 0)
+	{
+		scanf("%s",buffer);
+
+		num=strlen(buffer);
+		if(num>(size+1)) {
+
+			buffer[size+1] = '\0';
+			Result = size+1;
+		}
+		else {
+			buffer[num+1]='\0';
+			Result = num + 1;
+		}
+
+		for (num=0; num<Result; num++)
+		{  machine->WriteMem((addr+num), 1, (int) buffer[num]);
+			if (buffer[num] == '\0')
+			break; }
+		return num;
+
+	}
+	//read from a unix file, later you need change to nachos file system.
+	else
+	{
+		for(num=0;num<size;num++){
+			Read(id,&buffer[num],1);
+			machine->WriteMem((addr+num), 1, (int) buffer[num]);
+			if(buffer[num]=='\0') break;
+		}
+		return num;
+	}
+}
+
+
+
+static void SWrite(char *buffer, int size, int id)
+{
+	//write to terminal, try writting your own code using console class.
+	if (id == 1)
+	printf("%s", buffer);
+	//write to a unix file, later you need change to nachos file system.
+	if (id >= 2)
+	WriteFile(id,buffer,size);
+}
+
+// end FA98
+
