86224312777e939657fefd123552173adea4ee55
Alex Fontenot <jdafontenot@gmail.com>
2016-04-21 18:04:13 -0500

    It's purtty too

--- code/filesys/filehdr.cc
+++ code/filesys/filehdr.cc
@@ -47,7 +47,7 @@ FileHeader::Allocate(BitMap *freeMap, int fileSize)
     numBytes = fileSize;
     numSectors  = divRoundUp(fileSize, SectorSize);
     if (freeMap->NumClear() < numSectors)
-	return FALSE;		// not enough space
+        return FALSE;		// not enough space
     if(numBytes <= DirectLimit){
         for (int i = 0; i < numSectors; i++)
             dataSectors[i] = freeMap->Find();
@@ -73,6 +73,65 @@ FileHeader::Allocate(BitMap *freeMap, int fileSize)
             printf("Sector stored correctly with value of %d\n", data[i]);
         }
 
+    }else {
+        for (int i = 0; i < NumDirect ; i++)
+            dataSectors[i] = freeMap->Find();
+        int remainingSectors = numSectors - (NumDirect-2);
+        printf("Number of Sectors is: %d\n", numSectors);
+        printf("Remaining Sectors is: %d\n", remainingSectors);
+        printf("Indirect pointer stores value of %d\n", dataSectors[NumDirect-2]);
+        
+        //Takes care of our indirect pointer, which is full
+        int extraSectors[SectorSize/sizeof(int)]; //64
+
+        for (int i = 0; i < SectorSize/sizeof(int); i++){
+            extraSectors[i] = freeMap->Find();
+            printf("Found a sector at %d\n", extraSectors[i]);
+        }
+        synchDisk->WriteSector(dataSectors[NumDirect-2], (char *)extraSectors); //Write indirect pointer data
+
+        printf("Double Indirect pointer stores value of %d\n", dataSectors[NumDirect-1]);
+        int remainingIndirect = remainingSectors-64;
+        printf("We have %d remaining sectors\n", remainingIndirect);
+        //Now we need to handle the doubly-indirect pointer 
+        int indirectNeeded = remainingIndirect/64;
+        int indirectPointers[64];
+        //Round up to handle excess
+        if((remainingIndirect%64) != 0)
+            indirectNeeded += 1;
+        for(int i = 0; i < indirectNeeded; i++){
+            //Allocate space for each indirect pointer
+            indirectPointers[i] = freeMap->Find();
+            printf("Found a sector at %d\n", indirectPointers[i]);
+        }
+        //Write that list of indirect pointers to disk
+
+        synchDisk->WriteSector(dataSectors[NumDirect-1], (char *)indirectPointers); 
+        printf("Just wrote indirect pointers to disk\n");
+        printf("Now we need to allocate for each one's direct pointers\n");
+        //Now we need to write the data for each indirect pointer
+        for(int i = 0; i < indirectNeeded; i++){
+            //Write the sectors for the ith pointer
+            if(remainingIndirect < 64){
+                //Handle rest of sectors
+                int rSectors[64];
+                for(int r = 0; r < remainingIndirect; r++){
+                   rSectors[r] = freeMap->Find(); 
+                    printf("Found a sector at %d\n", rSectors[r]);
+                }
+                synchDisk->WriteSector(indirectPointers[i], (char *)rSectors); 
+                printf("Just wrote indirect %dths direct pointers\n");
+            }
+            else {
+                //This indirect pointer has 64 sectors to write
+                int rSectors[64];
+                for(int r = 0; r < 64; r++){
+                   rSectors[r] = freeMap->Find(); 
+                }
+                synchDisk->WriteSector(indirectPointers[i], (char *)rSectors); 
+                remainingIndirect -= 64;
+            }
+        }
     }
     return TRUE;
 }
@@ -84,6 +143,7 @@ FileHeader::Allocate(BitMap *freeMap, int fileSize)
 //	"freeMap" is the bit map of free disk sectors
 //----------------------------------------------------------------------
 
+//todo
 void 
 FileHeader::Deallocate(BitMap *freeMap)
 {
@@ -142,8 +202,42 @@ FileHeader::ByteToSector(int offset)
         else{
             int *data = new int[64];
             synchDisk->ReadSector(dataSectors[NumDirect-1], (char*)data);
-            int remainingSectors = numSectors - NumDirect;
-            return data[offset/SectorSize - NumDirect];
+            return data[offset/SectorSize - (NumDirect-1)];
+        }
+    }else {
+        if(offset/SectorSize < (NumDirect-2)){
+            return dataSectors[offset / SectorSize];
+        }
+        else if(offset/SectorSize >= (NumDirect-2) &&
+                (offset/SectorSize < 64+NumDirect-2)) {
+            //printf("Called byte to sector\n");
+            int *data = new int[64];
+            synchDisk->ReadSector(dataSectors[NumDirect-2], (char*)data);
+            int directSector = offset/SectorSize - (NumDirect-2);
+            //printf("Looking for the sector %d in the indirect pointer\n", directSector);
+            //printf("Think the data is stored at %d\n", data[directSector]);
+            return data[directSector];
+        } else {
+            //Handle double indirect translation
+            int *indirectPointers= new int[64];
+            synchDisk->ReadSector(dataSectors[NumDirect-1], (char*)indirectPointers);
+            //Which sector do we want out of the 64^2 stored by
+            //the doubly indirect pointer?
+            int whichDirectSector = (offset/SectorSize - (NumDirect-2) - 64);
+            //printf("We're looking for sector %d in DL\n", whichDirectSector);
+            //To which indirect pointer does that sector belong?
+            int whichIndirect = (whichDirectSector)/64;
+            //printf("We think it is in the %dth indirect sector\n", whichIndirect);
+            //printf("That indirect points us to sector %d\n", indirectPointers[whichIndirect]);
+            //What is the local offset for that sector
+            //in the indirect pointer's 64 possible direct pointers?
+            int directOffset = whichDirectSector%64;
+
+            //Load that indirect pointer's data here
+            int *directPointers = new int[64];
+            synchDisk->ReadSector(indirectPointers[whichIndirect], (char*)directPointers);
+            //Return the sector pointed to by the directOffset
+            return directPointers[directOffset];
         }
     }
 }
